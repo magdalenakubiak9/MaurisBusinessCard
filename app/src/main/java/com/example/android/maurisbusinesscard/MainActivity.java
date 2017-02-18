f12335c5ef3b7e57f4d02e4b0bc6a9ea91c181ab
Magdalena Kubiak <Magdalena Kubiak>
2017-02-18 08:07:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/maurisbusinesscard/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.maurisbusinesscard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
